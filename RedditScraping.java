9f7962210794902054cf76fac2397621ceadb8cd
Sarra <sarra.amara@icloud.com>
2021-07-15 13:07:35 +0200

    Display top comments of each post

--- RedditScraping.java
+++ RedditScraping.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,17 +9,18 @@ public class RedditScraping {
 
 	Document document = null;
 	Elements posts = null;
+	Elements comments = null;
 	
 	public RedditScraping(String subreddit) {
 		
 		
 		try {
 			String full_url= "https://www.reddit.com/r/" + subreddit;
-			document = Jsoup.connect(full_url).timeout(1000000).get();
+			document = Jsoup.connect(full_url).timeout(0).maxBodySize(0).get();
 			posts = document.getElementsByClass("SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE");
 			
 		}
-		catch(Exception e) {
+		catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -30,12 +33,31 @@ public class RedditScraping {
             String[] tokens = href.split("/");
           
             System.out.println("*******************************************************************");
-            System.out.println(tokens[2] + " ------ " + tokens[tokens.length-1]);
             System.out.println("Link: \n" + full_link);
+            System.out.println(tokens[2] + " ------ " + tokens[tokens.length-1]);
+            System.out.println("Comments : ");
+            printComments(full_link);
             System.out.println("*******************************************************************");
         }
        
     }
+	public void printComments(String link) {
+		try {
+			document = Jsoup.connect(link).get();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		comments = document.getElementsByClass("_3tw__eCCe7j-epNCKGXUKk _3jJ0c2FXVItDFjup-54-X2");
+		for (Element comment : comments) {
+			if(!comment.getElementsByClass("_1qeIAgB0cPwnLhDF9XSiJM").isEmpty()) {
+			System.out.println(" -"+comment.getElementsByClass("_1qeIAgB0cPwnLhDF9XSiJM").get(0).text());
+			}
+		}
+		
+		
+	}
 	
 	public static void main(String[] args){
         RedditScraping rs = new RedditScraping("all");
